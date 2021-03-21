e752617434e37c375b9e8fd7ea9e9eb09901c8c3
Shinobu <lvxiaoyan1998@hotmail.com>
2021-03-21 15:12:03 +0800

    add variable parameter function support

--- kern.c
+++ kern.c
@@ -6,8 +6,7 @@ void kern()
     /*for(int i = 0; i < 2500; i++)
         putc('0' + (i % 10), 0x07);*/
 
-    putc('\n', 0x07);
-    putc('1',0x07);
+    putc(sum(4,1,1,1,1) + '0',0x07);
     spin: 
         goto spin;
 }
--- lib.h
+++ lib.h
@@ -1,5 +1,11 @@
 #include "types.h"
 
 
+typedef char *  va_list; 
+#define _ALIGNSIZE(arg)     ((sizeof(arg) + sizeof(int) - 1) & ~(sizeof(int) - 1))
+#define va_start(ap,v)      (ap = (va_list)&v + _ALIGNSIZE(v))
+#define va_arg(ap,t)        (*(t *)((ap += _ALIGNSIZE(t)) - _ALIGNSIZE(t)))
+#define va_end(ap)          (ap=(va_list)0)  
+
 void memset(void* p, int c, uint32_t size);
 void memcpy(void* dst, void* src, uint32_t size);
